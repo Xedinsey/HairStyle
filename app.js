7391a18ea6a25c22c8689fa551f93eef4ba51357
Xedinsey <gameeter@gmail.com>
2021-12-08 17:01:52 +0300

    2

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
 const express = require('express')
+const {engine} = require('express-handlebars')
+
 
 const app = express()
-
 const port = process.env.PORT || 3000
 
+
+// data
+const fortunes = [
+    "Победи свои страхи, или они победят тебя." ,
+    "Рекам нужны истоки." ,
+    "Не бойся неведомого.",
+    "Тебя ждет приятный сюрприз.",
+    "Будь проще везде, где только можно.",
+]
+
+
+// Настройка механизма представлений Handlebars.
+app.engine('handlebars', engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'))
+
 // Домашняя страница
-app.get('/', (req, res) => {
-    res.type('text/plain')
-    res.send('Домашняя страница')
-})
+app.get('/', (req, res) => res.render('home'))
 
 // Страница "О нас"
 app.get('/about', (req, res) => {
-    res.type('text/plain')
-    res.send('Страница о нас')
+    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+    res.render('about' , { fortune: randomFortune })
 })
 
 // Пользовательская страница 404
 app.use((req, res) => {
-    res.type('text/plain')
     res.status(404)
-    res.send('404 - Страница не найдена')
+    res.render('404')
 })
 
 // Пользовательская страница 500
 app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.type('text/plain')
     res.status(500)
-    res.send('500 - Внутренняя ошибка сервера.')
+    res.render('500')
 })
 
+
+// Слушатель порта
 app.listen(port, () => console.log(
     `Express запущен на http://localhost:${port}; ` + `Нажмите Ctrl+C для завершения.`
 ))

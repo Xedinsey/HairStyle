27c91e1a30480f01b200a21a61c2ca2846e20867
Xedinsey <gameeter@gmail.com>
2021-12-09 09:56:02 +0300

    Рефактор. Добавил в представления футер и хедер.

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
+// Импорты
 const express = require('express')
 const {engine} = require('express-handlebars')
 const {getFortune} = require("./lib/fortune")
+const hbs = require('hbs')
 
-
+// Точка старта
 const app = express()
 const port = process.env.PORT || 3000
 
 
-
+// Настройки
 // Настройка механизма представлений Handlebars.
 app.engine('hbs', engine({defaultLayout: 'main'}))
 // app.set('view engine', 'handlebars')
 
 app.set('view engine', 'hbs')
+hbs.registerPartials(__dirname + '/views/partials')
 app.use(express.static(__dirname + '/public'))
 
-// Домашняя страница
-app.get('/', (req, res) => res.render('index'))
-
+// Роуты
+// Страница О нас
 app.get('/about', (req, res) => res.render('about'))
-
+// Страница блога
 app.get('/blog', (req, res) => res.render('blog'))
-
+// Страница контакты
 app.get('/contact', (req, res) => res.render('contact'))
-
+// Страница услуги
 app.get('/services', (req, res) => res.render('services'))
+// Домашняя страница
+app.get('/', (req, res) => res.render('home'))
 
-
-// Страница "О нас"
+// Страница "О нас из учебника"
 // app.get('/about', (req, res) => {
 //     res.render('about' , { fortune: getFortune() })
 // })
